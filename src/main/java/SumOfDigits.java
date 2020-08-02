d74eb4b3011fd0f15051a6bee45a3949ccd14f1b
wmaiouiru <wmaiouiru@gmail.com>
2020-08-02 08:54:20 -0700

    Added test to test `System.out.print`, but I need to figure out how to invoke @before and @after in unit test

--- src/main/java/SumOfDigits.java
+++ src/main/java/SumOfDigits.java
@@ -16,15 +16,15 @@ public class SumOfDigits {
         );
     }
     public static void ReverseNumber(){
-        System.out.println("start ReverseNumber");
+        // System.out.println("start ReverseNumber");
         Scanner scanner = new Scanner(System.in);
         String n = scanner.nextLine();
-        System.out.println("input: "+ n);
+        // System.out.println("input: "+ n);
         int out = 0;
         out += Integer.parseInt(n.substring(0,1));
         out += Integer.parseInt(n.substring(1,2)) * 10;
         out += Integer.parseInt(n.substring(2,3)) * 100;
-        System.out.println(out);
-        System.out.println("end ReverseNumber");
+        System.out.print(out);
+        // System.out.println("end ReverseNumber");
     }
 }
--- src/test/java/SumOfDigitsTest.java
+++ src/test/java/SumOfDigitsTest.java
@@ -1,17 +1,44 @@
 package test.java;
 
 import main.java.SumOfDigits;
+import org.junit.*;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.io.PrintStream;
 
-class SumOfDigitsTest {
+public class SumOfDigitsTest {
+    // reference: https://stackoverflow.com/questions/1119385/junit-test-for-system-out-println
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    public void setUpStreams() {
+        System.out.println("setUpStreams");
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    public void restoreStreams() {
+        System.out.println("restoreStreams");
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
 
     @org.junit.jupiter.api.Test
-    void reverseNumber() {
-        InputStream inputStream = System.in;
+    public void reverseNumber() {
+        // TODO need to be able to invoke before and after
+        setUpStreams();
+        InputStream sysInBackup = System.in;
         ByteArrayInputStream in = new ByteArrayInputStream("320".getBytes());
         System.setIn(in);
         SumOfDigits.ReverseNumber();
+        Assert.assertEquals("23", outContent.toString());
+        // System.out.println("outContent:" + outContent);
+        // System.out.println("errContent:" + errContent);
+        System.setIn(sysInBackup);
+        restoreStreams();
     }
 }
